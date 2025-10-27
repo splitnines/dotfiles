00b337c4b912f8dc6793a97eb345cb67c83508a0
Rickey Ingrassia <splitnines@gmail.com>
2025-10-26 20:03:11 -0700

    chore: a little cleaning in the nvim config

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -75,6 +75,7 @@ vim.keymap.set("n", "<C-h>", "<C-w><C-h>")
 vim.keymap.set("n", "<C-l>", "<C-w><C-l>")
 vim.keymap.set("n", "<C-j>", "<C-w><C-j>")
 vim.keymap.set("n", "<C-k>", "<C-w><C-k>")
+vim.keymap.set("n", "<leader>x", "<C-w>q")
 
 -- Scroll centering
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
@@ -128,6 +129,7 @@ require("lazy").setup({
     { "tpope/vim-sleuth" },
     { "folke/neodev.nvim", opts = {} },
     { "lukas-reineke/virt-column.nvim", opts = {} },
+
     -- git status in the gutter
     {
       "lewis6991/gitsigns.nvim",
