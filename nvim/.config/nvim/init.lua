1a4a12f442a7847b3b7c1e8b47de4b996fd843c8
Rickey Ingrassia <splitnines@gmail.com>
2025-10-27 21:44:26 -0700

    Changed the smear settings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -682,7 +682,14 @@ require("lazy").setup({
       "sphamba/smear-cursor.nvim",
       event = "VeryLazy",
       opts = {
-        -- Optional configuration (these are safe defaults)
+        stiffness = 0.8,
+        trailing_stiffness = 0.6,
+        stiffness_insert_mode = 0.7,
+        trailing_stiffness_insert_mode = 0.7,
+        damping = 0.95,
+        damping_insert_mode = 0.95,
+        distance_stop_animating = 0.5,
+        time_interval = 7,
         smear_between_buffers = true,
         smear_between_neighbor_lines = true,
         smear_insert_mode = true,
