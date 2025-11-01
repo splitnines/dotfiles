654c45ba1be8ba09f9acd168062072ff6ed03869
Rickey Ingrassia <splitnines@gmail.com>
2025-10-31 18:57:00 -0700

    nvim: added todo-comments plugin

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -187,6 +187,30 @@ require("lazy").setup({
     { "folke/neodev.nvim", opts = {} },
     { "lukas-reineke/virt-column.nvim", opts = {} },
 
+    -- Highlight NOTE, TODO, FIX, etc. in comments
+    {
+      "folke/todo-comments.nvim",
+      dependencies = { "nvim-lua/plenary.nvim" },
+      event = { "BufReadPost", "BufNewFile" },
+      opts = {
+        keywords = {
+          NOTE = { icon = "📝", color = "hint", alt = { "INFO" } },
+          TODO = { icon = "", color = "info" },
+          FIX = { icon = "", color = "error", alt = { "BUG", "FIXME" } },
+        },
+        comments_only = true,
+      },
+      config = function(_, opts)
+        require("todo-comments").setup(opts)
+        vim.keymap.set("n", "]t", function()
+          require("todo-comments").jump_next()
+        end, { desc = "Next TODO/NOTE" })
+        vim.keymap.set("n", "[t", function()
+          require("todo-comments").jump_prev()
+        end, { desc = "Prev TODO/NOTE" })
+      end,
+    },
+
     -- git status in the gutter
     {
       "lewis6991/gitsigns.nvim",
