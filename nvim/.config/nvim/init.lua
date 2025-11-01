d5045fb221e9e8eeca5f86acb2fc3a615c546c6b
Rickey Ingrassia <splitnines@gmail.com>
2025-10-31 19:08:01 -0700

    nvim: fixed cmp <tab> completion bahavior

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -641,7 +641,24 @@ require("lazy").setup({
             ["<C-b>"] = cmp.mapping.scroll_docs(-4),
             ["<C-f>"] = cmp.mapping.scroll_docs(4),
             ["<C-Space>"] = cmp.mapping.complete({}),
-            ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+            -- ["<TAB>"] = cmp.mapping.confirm({ select = true }),
+            ["<Tab>"] = cmp.mapping(function(fallback)
+              if cmp.visible() then
+                cmp.select_next_item()
+              elseif luasnip.expand_or_jumpable() then
+                luasnip.expand_or_jump()
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
+
+            ["<S-Tab>"] = cmp.mapping(function(fallback)
+              if luasnip.jumpable(-1) then
+                luasnip.jump(-1)
+              else
+                fallback()
+              end
+            end, { "i", "s" }),
           }),
           sources = {
             { name = "nvim_lsp" },
