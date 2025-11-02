132a38426bca26ff9ba8876f8d843e9f5ce23437
Rickey Ingrassia <splitnines@gmail.com>
2025-11-02 06:27:38 -0800

    nvim: removed 2 unused key bindings

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -532,14 +532,6 @@ require("lazy").setup({
         vim.keymap.set("n", "<C-s>", function()
           harpoon:list():select(4)
         end)
-
-        -- Toggle previous & next buffers stored within Harpoon list
-        vim.keymap.set("n", "<C-S-P>", function()
-          harpoon:list():prev()
-        end)
-        vim.keymap.set("n", "<C-S-N>", function()
-          harpoon:list():next()
-        end)
       end,
     },
 
