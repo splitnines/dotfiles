27557cf33a8075d2a2b89145482f2519ebae4c46
Rickey Ingrassia <splitnines@gmail.com>
2025-11-08 18:06:16 -0800

    nvim: trying to get the goto command working FFS

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -6,3 +6,14 @@ require("core.keymaps")
 require("core.autocmds")
 require("plugins.init")
 require("plugins.extras")
+
+-- Quiet known harmless deprecation messages without overwriting type info
+do
+  local orig_deprecate = vim.deprecate
+  rawset(vim, "deprecate", function(name, alternative, version, plugin)
+    if name == "client.supports_method" or name == "vim.validate{<table>}" then
+      return -- suppress only these warnings
+    end
+    return orig_deprecate(name, alternative, version, plugin)
+  end)
+end
