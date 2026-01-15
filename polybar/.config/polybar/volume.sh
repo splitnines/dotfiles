b131ef5fe34e3dca55f4d68bce2466d9215ddefb
Rickey Ingrassia <splitnines@gmail.com>
2026-01-15 14:28:45 -0800

    polybar: added volume control to mouse, i3: added volume controls to F1, F2 and F3

---
+++ polybar/.config/polybar/volume.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+
+SINK="@DEFAULT_AUDIO_SINK@"
+
+VOLUME=$(wpctl get-volume "$SINK")
+PERCENT=$(echo "$VOLUME" | awk '{print int($2 * 100)}')
+
+if echo "$VOLUME" | grep -q MUTED; then
+    echo "  muted"
+elif [ "$PERCENT" -eq 0 ]; then
+    echo "  0%"
+else
+    echo "   ${PERCENT}%"
+fi
+
