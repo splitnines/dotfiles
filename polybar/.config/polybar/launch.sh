eef20d8c485b0aa330d1b14619805c0d3a96fb8a
Rickey Ingrassia <splitnines@gmail.com>
2026-04-22 08:32:26 -0700

    i3/polybar: cleaned up the config

--- polybar/.config/polybar/launch.sh
+++ polybar/.config/polybar/launch.sh
@@ -3,9 +3,15 @@
 killall -q polybar
 
 while pgrep -u "$UID" -x polybar >/dev/null; do
-    sleep 0.5
+    sleep 0.2
 done
 
+PRIMARY="$(polybar --list-monitors | awk '/ \(primary\)/ {print $1}' | cut -d: -f1)"
+
 for m in $(polybar --list-monitors | cut -d":" -f1); do
-    MONITOR=$m polybar main &
+    if [ "$m" = "$PRIMARY" ]; then
+        MONITOR="$m" polybar main &
+    else
+        MONITOR="$m" polybar main-secondary &
+    fi
 done
